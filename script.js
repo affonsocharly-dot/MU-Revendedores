edf832e3967bdd938c79d3d0f8de6c565394d464
affonsocharly-dot <affonsocharly@gmail.com>
2026-07-23 08:22:19 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// MU Revendedores
+console.log("Site MU Revendedores carregado!");
+
+document.querySelector("button").addEventListener("click", function() {
+    alert("Pré-lançamento MU Revendedores em breve! 🚀");
+});
